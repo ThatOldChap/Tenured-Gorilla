1adca8c8e1841034c7a0cafec09824a2fda8f132
ThatOldChap <michaeljchap@gmail.com>
2018-06-23 14:16:41 -0400

    - Added Recent Spending activity

---
+++ app/src/main/java/com/michaelchaplin/spendometer/RecentSpendingActivity.java
@@ -0,0 +1,15 @@
+package com.michaelchaplin.spendometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecentSpendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recent_spending);
+    }
+
+
+}
