bbb64902f32e580dbf637b4fa20e9f0978cb8a5c
ThatOldChap <michaeljchap@gmail.com>
2018-06-28 01:46:22 -0400

    Started CategoryEditorActivity edits

--- app/src/main/java/com/michaelchaplin/spendometer/CategoryEditorActivity.java
+++ app/src/main/java/com/michaelchaplin/spendometer/CategoryEditorActivity.java
@@ -1,13 +1,31 @@
 package com.michaelchaplin.spendometer;
 
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CategoryEditorActivity extends AppCompatActivity {
+public class CategoryEditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_editor);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
